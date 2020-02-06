2c18906bd607b12d4b42eff07f790a7572f01068
Leandro Acquati <leandroacquati@gmail.com>
2020-02-06 01:55:11 -0300

    add multiple slides

--- script-dinamic.js
+++ script-dinamic.js
@@ -1,65 +1,44 @@
-var slides = document.getElementsByClassName('slide');
-
-if (slides.length) {
-  var slideShow = [];
+function nextSlide(slideNumber) {
+	let slides = document.getElementsByClassName('slide' + slideNumber);
 
   for (let i = 0; i < slides.length; i++) {
-    slideShow.push({
-      backgroundImage: slides[i].style.backgroundImage,
-      currentSlide: false
-    });
-  }
-
-  slideShow[0].currentSlide = true;
-  slides[0].classList.toggle('current-slide');
-}
-
-var bleu = document.getElementsByClassName('slide');
-
-function nextSlide() {
-  for (let i = 0; i < slides.length; i++) {
-    if (slideShow[i].currentSlide === true) {
-      slideShow[i].currentSlide = false;
-      document.getElementsByClassName('w3-button')[0].disabled = true;
-      document.getElementsByClassName('w3-button')[1].disabled = true;
-
-      if (!slides[i].classList.contains('current-slide')) {
-        slides[i].classList.toggle('current-slide');
-      }
+    if (slides[i].classList.contains('current-slide')) {
+      document.getElementsByClassName('slide' + slideNumber + '-button')[0].disabled = true;
+      document.getElementsByClassName('slide' + slideNumber + '-button')[1].disabled = true;
 
       if (i === slides.length - 1) {
-        slideShow[0].currentSlide = true;
         slides[0].classList.toggle('next-slide');
         slides[0].style.zIndex = '200';
 
         setTimeout(function () {
           slides[0].classList.remove('next-slide');
           slides[0].classList.toggle('current-slide');
-        }, 0);
+        }, 10);
 
         setTimeout(function () {
           slides[slides.length - 1].classList.remove('current-slide');
-          slides[0].style.zIndex = 'auto';
-          document.getElementsByClassName('w3-button')[0].disabled = false;
-          document.getElementsByClassName('w3-button')[1].disabled = false;
+					slides[0].style.zIndex = 'auto';
+					
+          document.getElementsByClassName('slide' + slideNumber + '-button')[0].disabled = false;
+          document.getElementsByClassName('slide' + slideNumber + '-button')[1].disabled = false;
         }, 1000);
 
         break;
       } else {
-        slideShow[i + 1].currentSlide = true;
         slides[i + 1].classList.toggle('next-slide');
         slides[i + 1].style.zIndex = '200';
 
         setTimeout(function () {
           slides[i + 1].classList.remove('next-slide');
           slides[i + 1].classList.toggle('current-slide');
-        }, 0);
+        }, 10);
 
         setTimeout(function () {
           slides[i].classList.remove('current-slide');
-          slides[i + 1].style.zIndex = 'auto';
-          document.getElementsByClassName('w3-button')[0].disabled = false;
-          document.getElementsByClassName('w3-button')[1].disabled = false;
+					slides[i + 1].style.zIndex = 'auto';
+					
+          document.getElementsByClassName('slide' + slideNumber + '-button')[0].disabled = false;
+          document.getElementsByClassName('slide' + slideNumber + '-button')[1].disabled = false;
         }, 1000);
 
         break;
@@ -68,50 +47,45 @@ function nextSlide() {
   }
 }
 
-function previousSlide() {
-  for (let i = 0; i < slides.length; i++) {
-    if (slideShow[i].currentSlide === true) {
-      slideShow[i].currentSlide = false;
-      document.getElementsByClassName('w3-button')[0].disabled = true;
-      document.getElementsByClassName('w3-button')[1].disabled = true;
+function previousSlide(slideNumber) {
+	let slides = document.getElementsByClassName('slide' + slideNumber);
 
-      if (!slides[i].classList.contains('current-slide')) {
-        slides[i].classList.toggle('current-slide');
-      }
+  for (let i = 0; i < slides.length; i++) {
+    if (slides[i].classList.contains('current-slide')) {
+      document.getElementsByClassName('slide' + slideNumber + '-button')[0].disabled = true;
+      document.getElementsByClassName('slide' + slideNumber + '-button')[1].disabled = true;
 
       if (i === 0) {
-        slideShow[slides.length - 1].currentSlide = true;
         slides[slides.length - 1].classList.toggle('previous-slide');
         slides[slides.length - 1].style.zIndex = '200';
 
         setTimeout(function () {
           slides[slides.length - 1].classList.remove('previous-slide');
           slides[slides.length - 1].classList.toggle('current-slide');
-        }, 0);
+        }, 10);
 
         setTimeout(function () {
           slides[0].classList.remove('current-slide');
           slides[slides.length - 1].style.zIndex = 'auto';
-          document.getElementsByClassName('w3-button')[0].disabled = false;
-          document.getElementsByClassName('w3-button')[1].disabled = false;
+          document.getElementsByClassName('slide' + slideNumber + '-button')[0].disabled = false;
+          document.getElementsByClassName('slide' + slideNumber + '-button')[1].disabled = false;
         }, 1000);
 
         break;
       } else {
-        slideShow[i - 1].currentSlide = true;
         slides[i - 1].classList.toggle('previous-slide');
         slides[i - 1].style.zIndex = '200';
 
         setTimeout(function () {
           slides[i - 1].classList.remove('previous-slide');
           slides[i - 1].classList.toggle('current-slide');
-        }, 0);
+        }, 10);
 
         setTimeout(function () {
           slides[i].classList.remove('current-slide');
           slides[i - 1].style.zIndex = 'auto';
-          document.getElementsByClassName('w3-button')[0].disabled = false;
-          document.getElementsByClassName('w3-button')[1].disabled = false;
+          document.getElementsByClassName('slide' + slideNumber + '-button')[0].disabled = false;
+          document.getElementsByClassName('slide' + slideNumber + '-button')[1].disabled = false;
         }, 1000);
 
         break;
